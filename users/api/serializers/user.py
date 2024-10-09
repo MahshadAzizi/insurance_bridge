afcc4e155f96858d2854d48c6d0ca3c3e9b01dc3
mahshad <azizi.mhshd@gmail.com>
2024-10-09 15:33:50 +0330

    feat: Add user serializer

---
+++ users/api/serializers/user.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'father_name',
+            'national_code',
+            'place_of_issue',
+            'mobile_number',
+            'birth_date',
+        ]
